5ba62ff7884d2fd07f276dfd4ee6f188ce571d98
shixianqing <1434501995@qq.com>
2019-07-17 19:33:40 +0800

    gitignore

--- src/main/java/com/insuresmart/producer/CustomerProducer.java
+++ src/main/java/com/insuresmart/producer/CustomerProducer.java
@@ -1,5 +1,6 @@
 package com.insuresmart.producer;
 
+import com.insuresmart.utils.PropsUtils;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
@@ -17,19 +18,19 @@ public class CustomerProducer {
         Properties props = new Properties();
 
         //kafka集群
-        props.put("bootstrap.servers","192.168.86.131:9092,192.168.86.132:9092,192.168.86.134:9092");
-        props.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer","org.apache.kafka.common.serialization.StringSerializer");
+        props.put("bootstrap.servers", PropsUtils.getString("bootstrap.servers"));
+        props.put("key.serializer",PropsUtils.getString("key.serializer"));
+        props.put("value.serializer",PropsUtils.getString("value.serializer"));
         //等待所有副本节点的应答
-        props.put("acks","all");
+        props.put("acks",PropsUtils.getString("acks"));
         // 消息发送失败最大尝试次数
-        props.put("retries","0");
+        props.put("retries",PropsUtils.getString("retries"));
         //一次批处理消息大小
-        props.put("batch.size","16384");
+        props.put("batch.size",PropsUtils.getString("batch.size"));
         //请求延时
-        props.put("linger.ms","5");
+        props.put("linger.ms",PropsUtils.getString("linger.ms"));
         //发送缓存区内存大小
-        props.put("buffer.memory","33554432");
+        props.put("buffer.memory",PropsUtils.getString("buffer.memory"));
 
         KafkaProducer<String,String> producer = new KafkaProducer<String, String>(props);
 
